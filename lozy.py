5a736a5275f111ac9b65ff1b6fb38f7d4c361b84
akram sadeghi <103222224+akramsadeghi@users.noreply.github.com>
2022-10-04 17:30:00 +0330

    Add files via upload

---
+++ lozy.py
@@ -0,0 +1,13 @@
+
+
+n= int(input('andazeh zeleh lozi ra vard konid:  '))
+
+
+for i in range(1,n+1):
+    print((n-i)*' '+(2*i-1)*'*')
+for i in range(1,n+1):
+    print((i)*' '+(-2*i+(2*n-1))*'*')    
+
+#for i in range(1)    
+    #for i in range(n-2,0):
+       # print(" ","*")
